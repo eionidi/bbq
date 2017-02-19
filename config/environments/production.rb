e8085b8df0b089a0d532b1d71b97fc2a19729f52
Catherine <eionidi@artec-group.com>
2017-02-19 16:33:38 +0300

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,8 +86,8 @@ Rails.application.configure do
     :address              => 'smtp.sendgrid.net',
     :port                 => '587',
     :authentication       => :plain,
-    :user_name            => ENV['staging-support@artec-group.com'],
-    :password             => ENV['zdfcytpdfkblbnt'],
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
     :domain               => 'heroku.com',
     :enable_starttls_auto => true
   }
