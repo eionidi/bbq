4fbaac146ac041ae41cae257a9c2c5e034ef88b6
Catherine <eionidi@artec-group.com>
2017-02-21 23:14:02 +0300

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,8 +46,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
       port: '587',
-      user_name: 'cat81.06@gmail.com', 
-      password: 'boomerang1981',
+      user_name: '', 
+      password: '',
       authentication: 'plain',
       enable_starttls_auto: true}
 end
