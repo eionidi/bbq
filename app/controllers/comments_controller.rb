27d634b980a53c8efb4b66a60d2d809d0a20ecdd
Catherine <eionidi@artec-group.com>
2017-03-01 00:29:56 +0300

    notify users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -49,7 +49,7 @@ class CommentsController < ApplicationController
   end
   
   def notify_subscribers(event, comment)
-    all_emails = (event.subscriptions.map{|s| s.user_name unless s.user == notification.user} + [event.user.email]).compact
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email])
     all_emails.each do |mail|
       EventMailer.comment(event, comment, mail).deliver_now
     end
